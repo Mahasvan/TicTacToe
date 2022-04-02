294c578f9b9c2a34b92d7242dcbfbbe827eb570a
Mahas1 <mahasvan@gmail.com>
2022-04-02 10:17:23 +0530

    more error handling, move spec files to new folder if user doesnt want to delete

--- build_executable.py
+++ build_executable.py
@@ -25,14 +25,18 @@ print("Starting Build...")
 if sys.platform == "win32":
     # Build for Windows
     print("Building for Windows")
-    pyi.run([
-        "--name", "TicTacToe_windows",
-        "--clean",
-        "--onefile",
-        "--icon", "Images/tictactoe.ico",
-        "--log-level", "WARN",
-        "main.py"
-    ])
+    try:
+        pyi.run([
+            "--name", "TicTacToe_windows",
+            "--clean",
+            "--onefile",
+            "--icon", "Images/tictactoe.ico",
+            "--log-level", "WARN",
+            "main.py"
+        ])
+    except Exception as e:
+        print("{}: {}".format(type(e).__name__, e))
+
 elif sys.platform == "darwin":
     try:
         # Build x86_64
@@ -48,7 +52,8 @@ elif sys.platform == "darwin":
         ])
     except PyInstaller.utils.osx.IncompatibleBinaryArchError:
         print("x86_64 architecture not supported. Skipping...")
-
+    except Exception as e:
+        print("{}: {}".format(type(e).__name__, e))
     try:
         # Build ARM64
         print("Building ARM64 executable for macOS...")
@@ -63,6 +68,8 @@ elif sys.platform == "darwin":
         ])
     except PyInstaller.utils.osx.IncompatibleBinaryArchError:
         print("ARM64 architecture not supported. Skipping...")
+    except Exception as e:
+        print("{}: {}".format(type(e).__name__, e))
 
     try:
         # Build universal2
@@ -78,17 +85,23 @@ elif sys.platform == "darwin":
         ])
     except PyInstaller.utils.osx.IncompatibleBinaryArchError:
         print("Universal2 architecture not supported. Skipping...")
+    except Exception as e:
+        print("{}: {}".format(type(e).__name__, e))
 
 else:
     # Build for other OSes, probably Linux
     print("Building executable for Linux...")
-    pyi.run([
-        "--name", "TicTacToe",
-        "--clean",
-        "--onefile",
-        "--log-level", "WARN",
-        "main.py"
-    ])
+    try:
+        pyi.run([
+            "--name", "TicTacToe",
+            "--clean",
+            "--onefile",
+            "--log-level", "WARN",
+            "main.py"
+        ])
+    except Exception as e:
+        print("{}: {}".format(type(e).__name__, e))
+
 print("Built successfully! Clearing temporary files...")
 try:
     shutil.rmtree("./build")
@@ -97,7 +110,8 @@ except Exception as e:
     pass
 
 print("Done! Executable is located in the `dist` folder!")
-if input("Clean spec files? (y/n) ").lower() == "y":
+spec_clean = input("Clean spec files? (y/n): ").lower()
+if spec_clean == "y":
     spec_files = [f for f in os.listdir(".") if f.endswith(".spec")]
     print()
     for f in spec_files:
@@ -108,3 +122,16 @@ if input("Clean spec files? (y/n) ").lower() == "y":
             print(type(e).__name__, e)
             pass
     print("Spec files removed!")
+else:
+    print("Moving spec files to `spec_files` folder...")
+    spec_files = [f for f in os.listdir(".") if f.endswith(".spec")]
+    if not os.path.exists("spec_files"):
+        os.mkdir("spec_files")
+    for f in spec_files:
+        print("Moving spec file:", f)
+        try:
+            shutil.copy(f, "spec_files", )  # we copy and not move because if moving, it errors out if the file already exists
+        except Exception as e:
+            print(type(e).__name__, e)
+        os.remove(f)  # remove the original file after copying
+    print("Spec files moved!")
